845d38ae312db50678f63560981a9195ccf7f597
maxpanev <maxpanev170100@gmail.com>
2024-09-24 14:53:35 +0300

    add new code

--- main.py
+++ main.py
@@ -2,67 +2,32 @@ import time
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
 
-
 url = "https://www.divan.ru/krasnodar/category/divany-i-kresla"
 
-# Открываем веб-страницу
 driver.get(url)
 
-# Задаём 3 секунды ожидания, чтобы веб-страница успела прогрузиться
 time.sleep(3)
 
+divans = driver.find_elements(By.CLASS_NAME, '_Ud0k')
 
-# Находим все карточки с вакансиями с помощью названия класса
-# Названия классов берём с кода сайта
-divans = driver.find_elements(By.CLASS_NAME, 'LlPhw')
-
-
-
-# Выводим вакансии на экран
-print(divans)
-# Создаём список, в который потом всё будет сохраняться
 my_list = []
 
-
-
-# Перебираем коллекцию вакансий
-# Используем конструкцию try-except, чтобы "ловить" ошибки, как только они появляются
 for divan in divans:
-   try:
-   # Находим элементы внутри вакансий по значению
-   # Находим название
-     name = divan.find_element(By.CSS_SELECTOR, 'div.lsooF').text
-     # Находим цену
-     price = divan.find_element(By.CSS_SELECTOR, 'div.pY3d2').text
-     # Находим ссылку с помощью атрибута 'href'
-     #url = divan.find_element(By.CSS_SELECTOR, 'a.').get_attribute('href')
-   # Вставляем блок except на случай ошибки - в случае ошибки программа попытается продолжать
-   except:
-     print("произошла ошибка при парсинге")
-     continue
+    try:
+        name = divan.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
+        price = divan.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU').text
+        my_list.append([name, price])  # Перенесено внутрь цикла
+    except NoSuchElementException:
+        print("Произошла ошибка при парсинге")
+        continue
 
-
-
-# Вносим найденную информацию в список
-my_list.append([name, price])
-
-
-
-# Закрываем подключение браузер
 driver.quit()
 
-
-
-# Прописываем открытие нового файла, задаём ему название и форматирование
-# 'w' означает режим доступа, мы разрешаем вносить данные в таблицу
-with open("homework.csv", 'w',newline='', encoding='utf-8') as file:
-# Создаём объект
+with open("homework.csv", 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-# Создаём первый ряд
-    writer.writerow(['Название', 'Цена', 'Ссылка'])
-
-# Прописываем использование списка как источника для рядов таблицы
-writer.writerows(my_list)
+    writer.writerow(['Название', 'Цена'])
+    writer.writerows(my_list)  # Перенесено внутрь блока с открытием файла
